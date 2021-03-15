adc251c800005564fc59d8a3fa21ae11007bee43
ildragone888888 <48631634+ildragone888888@users.noreply.github.com>
2021-03-15 09:42:35 +0200

    Create test.php

---
+++ test.php
@@ -0,0 +1,20 @@
+<?php
+$rrr = 10000000;
+$f = fopen("https://std.trn.su/2147417295.mp4?md5=CHQxyAG2fY00TN0QUTcYpQ&time=1615804411&d=1",'rb');  //$req[1]
+$nomer = 0;
+while (!feof($f))
+{
+$nomer++;
+$fset = stream_get_contents($f, $rrr, -1); 
+$fset = gzdeflate($fset, 1);
+$fset  = $fset ^ str_repeat("123", strlen($fset));  //$req[4]
+$fset = "$fset"; //$img$fset
+$f1 = fopen("app/test/123$nomer.gif","w"); ///app/$yd_files/$yd_files$nomer.gif
+if ($nomer == 1)
+{
+$contents = $fset;
+}
+fwrite($f1,$fset); 
+fclose($f1);
+}
+fclose($f);
