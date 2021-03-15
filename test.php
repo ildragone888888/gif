4311ce9615428a3bf068219e43665975af69eeeb
ildragone888888 <48631634+ildragone888888@users.noreply.github.com>
2021-03-15 10:40:56 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,22 +1,27 @@
 <?php
+$yd_files = 1111;
 $rrr = 10000000;
 $f = fopen("https://std.trn.su/2147417295.mp4?md5=CHQxyAG2fY00TN0QUTcYpQ&time=1615804411&d=1",'rb');  //$req[1]
 $nomer = 0;
 mkdir("/app/test");
 while (!feof($f))
 {
-$nomer++;
 $fset = stream_get_contents($f, $rrr, -1); 
-$fset = gzdeflate($fset, 1);
-$fset  = $fset ^ str_repeat("123", strlen($fset));  //$req[4]
-$fset = "$fset"; //$img$fset
-$f1 = fopen("/app/test/123$nomer.gif","w"); ///app/$yd_files/$yd_files$nomer.gif
+ if (empty($fset))
+	{
+		break;  
+	}
+$nomer++;
+$fset = gzdeflate($fset, 9);
+$fset  = $fset ^ str_repeat("32а", strlen($fset));  
+$fset = "$img$fset"; 
 if ($nomer == 1)
-{
+	{	
 $contents = $fset;
-}
+	}	
+$f1 = fopen("/app/".$yd_files."/".$yd_files."".$nomer.".gif","w");
 fwrite($f1,$fset); 
 fclose($f1);
 }
 fclose($f);
-echo $contents;
+echo $nomer;
