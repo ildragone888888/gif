f0fc17937997f289c4b5024e873827053a3e5ceb
ildragone888888 <48631634+ildragone888888@users.noreply.github.com>
2021-04-01 10:32:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-echo "123 ya";
+<?php
+if(!isset($_POST['submit']))
+{
+echo "Быстрый сжиматель ya </br>
+<form enctype='multipart/form-data' action='indexx.php' method='POST'>
+<input type='hidden' name='MAX_FILE_SIZE' value='300000' />
+<input name='userfile' type='file' />
+ <label for='pwd'>Password:</label>
+<input type='password' id='pwd' name='pwd'> 
+<input type='submit' name='submit' value='Отправить файл' />
+</form>";
+}
+if(isset($_POST['submit']))
+{
+ echo "Error вы не авторизованы";
+}
